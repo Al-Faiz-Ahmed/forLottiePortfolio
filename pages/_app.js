ac720ddd2cc36322d292c7636285f0f1b1f20852
sheikhfaiz546 <sheikhfaiz546@gmail.com>
2022-09-02 23:45:58 +0500

    Next Conversion with footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-
+import Head from "next/head";
+import { Header, ThemeFirstLayer } from "../components/layout";
+import "../styles/general/main.scss";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+      </Head>
+        <ThemeFirstLayer />
+      <div className="pages-parent mx-auto bg-transparent">
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
